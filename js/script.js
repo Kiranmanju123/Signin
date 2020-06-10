4ed0e4c98a733def0389abcc59432ef05958dd5b
Kiran <kiramanju98@yhoo.com>
2020-06-10 13:40:35 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+   
+//     $(".signup_step2").hide();
+// });
+
+
+$("#btn1").click(function(){
+  $(".signup_step2").show();
+  $(".signup_step1").hide();
+
+});
+
+$("#btn2").click(function(){
+  $(".signup_step3").show();
+  $(".signup_step2").hide();
+
+});
